7bc80d72929717576f13ee149f7aeb61e7092f6e
Jordan Guinn <the.jordan.guinn@gmail.com>
2025-07-31 17:59:10 -0700

    Extend Tab Manager example

--- functional-samples/tutorial.tabs-manager/popup.js
+++ functional-samples/tutorial.tabs-manager/popup.js
@@ -12,12 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const tabs = await chrome.tabs.query({
-  url: [
-    'https://developer.chrome.com/docs/webstore/*',
-    'https://developer.chrome.com/docs/extensions/*'
-  ]
-});
+const tabs = await chrome.tabs.query({});
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
 const collator = new Intl.Collator();
@@ -25,18 +20,19 @@ tabs.sort((a, b) => collator.compare(a.title, b.title));
 
 const template = document.getElementById('li_template');
 const elements = new Set();
+
+document.querySelector('.tab-count').textContent = tabs.length;
+
 for (const tab of tabs) {
   const element = template.content.firstElementChild.cloneNode(true);
 
-  const title = tab.title.split('|')[0].trim();
-  const pathname = new URL(tab.url).pathname.slice('/docs'.length);
-
-  element.querySelector('.title').textContent = title;
-  element.querySelector('.pathname').textContent = pathname;
-  element.querySelector('a').addEventListener('click', async () => {
-    // need to focus window as well as the active tab
-    await chrome.tabs.update(tab.id, { active: true });
-    await chrome.windows.update(tab.windowId, { focused: true });
+  element.querySelector('.title').textContent = tab.title;
+  element.querySelector('.icon').src = tab.favIconUrl;
+  element.querySelector('button').addEventListener('click', async () => {
+    await chrome.tabs.remove(tab.id);
+    elements.delete(element);
+    element.remove();
+    document.querySelector('.tab-count').textContent -= 1;
   });
 
   elements.add(element);
